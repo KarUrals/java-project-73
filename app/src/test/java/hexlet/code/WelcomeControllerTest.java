6b24f4f331bdd8b94dfc2fff776299729251c9a8
Karelskiy_Andrey <karelskiy.a@gmail.com>
2023-04-27 10:31:57 +0300

    Rebuild project structure

--- app/src/test/java/hexlet/code/WelcomeControllerTest.java
+++ app/src/test/java/hexlet/code/WelcomeControllerTest.java
@@ -1,4 +1,4 @@
-package hexlet.code.app;
+package hexlet.code;
 
 import static org.hamcrest.Matchers.equalTo;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
