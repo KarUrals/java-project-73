5e91558c72c2f16653576091c10be5e60557a62e
Karelskiy_Andrey <karelskiy.a@gmail.com>
2023-04-28 18:08:00 +0300

    Add UserService to the project

---
+++ app/src/main/java/hexlet/code/service/UserServiceImpl.java
@@ -0,0 +1,38 @@
+package hexlet.code.service;
+
+import hexlet.code.dto.UserDto;
+import hexlet.code.entity.User;
+import hexlet.code.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Transactional
+@RequiredArgsConstructor
+public class UserServiceImpl implements UserService {
+
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+
+    @Override
+    public User createNewUser(UserDto userDto) {
+        final User user = new User();
+        user.setEmail(userDto.getEmail());
+        user.setFirstName(userDto.getFirstName());
+        user.setLastName(userDto.getLastName());
+        user.setPassword(passwordEncoder.encode(userDto.getPassword()));
+        return userRepository.save(user);
+    }
+
+    @Override
+    public User updateCurrentUser(long id, UserDto userDto) {
+        final User userToUpdate = userRepository.findById(id).get();
+        userToUpdate.setEmail(userDto.getEmail());
+        userToUpdate.setFirstName(userDto.getFirstName());
+        userToUpdate.setLastName(userDto.getLastName());
+        userToUpdate.setPassword(passwordEncoder.encode(userDto.getPassword()));
+        return userRepository.save(userToUpdate);
+    }
+}
