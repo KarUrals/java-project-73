84e8f72dbc630da18c939fe489ae1e4d073f2929
Karelskiy_Andrey <karelskiy.a@gmail.com>
2023-05-03 22:14:36 +0300

    Add loadUserByUsername() to UserServiceImpl.java

--- app/src/main/java/hexlet/code/service/UserServiceImpl.java
+++ app/src/main/java/hexlet/code/service/UserServiceImpl.java
@@ -4,14 +4,19 @@ import hexlet.code.dto.UserDto;
 import hexlet.code.entity.User;
 import hexlet.code.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import static hexlet.code.config.security.SecurityConfig.DEFAULT_AUTHORITIES;
+
 @Service
 @Transactional
 @RequiredArgsConstructor
-public class UserServiceImpl implements UserService {
+public class UserServiceImpl implements UserService, UserDetailsService {
 
     private final UserRepository userRepository;
     private final PasswordEncoder passwordEncoder;
@@ -34,5 +39,15 @@ public class UserServiceImpl implements UserService {
         userToUpdate.setLastName(userDto.getLastName());
         userToUpdate.setPassword(passwordEncoder.encode(userDto.getPassword()));
         return userRepository.save(userToUpdate);
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByEmail(username)
+                .orElseThrow(() -> new UsernameNotFoundException("Not found user with 'email': " + username));
+        return new org.springframework.security.core.userdetails.User(
+                user.getEmail(),
+                user.getPassword(),
+                DEFAULT_AUTHORITIES);
     }
 }
