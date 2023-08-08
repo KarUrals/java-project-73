05cdf235d56ccea5ce020823d28d107d4fd78c7f
Karelskiy_Andrey <karelskiy.a@gmail.com>
2023-08-08 08:48:33 +0300

    Exclude @ComponentScan

--- src/test/java/hexlet/code/config/SpringConfigForIT.java
+++ src/test/java/hexlet/code/config/SpringConfigForIT.java
@@ -1,8 +1,8 @@
 package hexlet.code.config;
 
-import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.context.annotation.Profile;
@@ -13,13 +13,13 @@ import static hexlet.code.config.SpringConfigForIT.TEST_PROFILE;
 @Configuration
 @Profile(TEST_PROFILE)
 @EnableAspectJAutoProxy
-@ComponentScan(basePackages = "hexlet.code")
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class SpringConfigForIT {
 
     public static final String TEST_PROFILE = "test";
 
     @Bean
     public RestTemplate restTemplate() {
-        return new RestTemplateBuilder().build();
+        return new RestTemplate();
     }
 }
